f3b3e5d946879ae9d545b782a3dd13048e315c40
mderkach <derkach94@gmail.com>
2020-04-20 16:48:31 +0300

    preview avatar script finish

--- src/js/modules/previewAvatar.js
+++ src/js/modules/previewAvatar.js
@@ -5,16 +5,37 @@ import {
 var previewAvatar = {
 	input: document.querySelector('#file'),
 	target: document.querySelector('.profile__form-input_file-button'),
+	avatar: {
+		img: document.querySelector('.profile__form-input_avatar'),
+		source: document.querySelector('.profile__form-input_avatar').parentElement.querySelector('source'),
+	},
+	buttonText: document.querySelector('.profile__form-input_file-p'),
 	events: {
 		choose: () => {
 			previewAvatar.input.click()
-			console.log('1')
 		},
-		change: elm => {
-			console.log(elm.files)
+		change: () => {
+			previewAvatar.changeImage();
 		}
 	},
-	changeImage: () => {},
+	changeImage: () => {
+		let loadedImage = URL.createObjectURL(event.target.files[0]);
+		previewAvatar.avatar.img.src = loadedImage;
+		previewAvatar.avatar.source.setAttribute('srcset', loadedImage);
+	},
+	checkImage: () => {
+		const target = previewAvatar.target;
+		const img = previewAvatar.avatar.img;
+		const text = previewAvatar.buttonText;
+
+		if (img.getAttribute('src') === '' || img.getAttribute('src') === '#' || img.getAttribute('src') === null || img.getAttribute('src') === undefined) {
+			target.classList.add('is-empty');
+			text.innerHTML = 'Загрузить фото';
+		} else {
+			target.classList.add('is-filled');
+			text.innerHTML = 'Заменить';
+		}
+	},
 	init: () => {
 		const target = previewAvatar.target;
 		const input = previewAvatar.input;
@@ -23,8 +44,12 @@ var previewAvatar = {
 				previewAvatar.events.choose()
 			})
 			input.addEventListener('change', () => {
-				previewAvatar.events.change(input)
+				previewAvatar.events.change();
+				previewAvatar.checkImage();
 			})
+
+			previewAvatar.checkImage();
+
 		}
 	}
 }
