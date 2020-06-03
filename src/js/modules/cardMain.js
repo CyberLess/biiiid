2e681dedc4a6108139b7f16a6f58263999b99954
mderkach <derkach94@gmail.com>
2020-06-03 16:08:28 +0300

    fix card behavior

--- src/js/modules/cardMain.js
+++ src/js/modules/cardMain.js
@@ -1,14 +1,10 @@
 var cardMain = {
 	card: document.querySelectorAll('.mainpage-order__card'),
-	viewport: Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0),
 	setActive: elm => {
 		elm.classList.add('is-active')
 	},
 	setInactive: elm => {
 		elm.classList.remove('is-active')
-	},
-	toggleActive: elm => {
-		elm.classList.toggle('is-active')
 	},
 	setAllInactive: array => {
 		array.forEach(elm => {
@@ -17,23 +13,23 @@ var cardMain = {
 	},
 	init: () => {
 		if (cardMain.card) {
+			let getViewport = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
 			cardMain.card.forEach(card => {
-				if (cardMain.viewport > 581) {
-					card.removeEventListener('click', () => {})
+				if (getViewport > 581) {
+					card.removeEventListener('click', () => { })
 					card.addEventListener('mouseover', () => { cardMain.setActive(card) })
 					card.addEventListener('mouseleave', () => { cardMain.setInactive(card) })
 				} else {
-					// let trigger = card.querySelector('.mainpage-order__card-header')
 					card.addEventListener('click', () => {
-						// cardMain.setAllInactive(cardMain.card)
-						cardMain.toggleActive(card)
+						card.classList.toggle('is-active')
 					})
 				}
 			})
+
 			window.addEventListener('resize', () => {
 				cardMain.init()
-				console.log('resized viewport:' + cardMain.viewport)
-			});
+			})
 		}
 	 }
 }
