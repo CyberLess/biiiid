413969dd3dddb8beef7402a55fb5411ec17b982c
mderkach <derkach94@gmail.com>
2020-06-04 12:10:50 +0300

    fixes

--- src/js/modules/cardMain.js
+++ src/js/modules/cardMain.js
@@ -6,33 +6,22 @@ var cardMain = {
 	setInactive: elm => {
 		elm.classList.remove('is-active')
 	},
-	setAllInactive: array => {
-		array.forEach(elm => {
-			elm.classList.remove('is-active')
-		})
-	},
 	init: () => {
 		if (cardMain.card) {
-			let getViewport = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-
-			cardMain.card.forEach(card => {
-				if (getViewport > 581) {
-					card.removeEventListener('click', () => { })
-					card.addEventListener('mouseover', () => { cardMain.setActive(card) })
-					card.addEventListener('mouseleave', () => { cardMain.setInactive(card) })
-				} else {
-					card.addEventListener('click', () => {
+				cardMain.card.forEach(card => {
+					card.addEventListener('mouseenter', () => {
+						cardMain.setActive(card)
+					})
+					card.addEventListener('mouseleave', () => {
+						cardMain.setInactive(card)
+					})
+					card.addEventListener('touchend', () => {
 						card.classList.toggle('is-active')
 					})
-				}
-			})
-
-			window.addEventListener('resize', () => {
-				cardMain.init()
-			})
+				})
+			}
 		}
 	 }
-}
 
 export {
 	cardMain
