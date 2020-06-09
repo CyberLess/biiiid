f51e840e9012dd9540b9ea517c8927b52d380623
mderkach <derkach94@gmail.com>
2020-06-09 14:18:32 +0300

    another script fix

--- src/js/modules/cardMain.js
+++ src/js/modules/cardMain.js
@@ -10,32 +10,27 @@ var cardMain = {
 		console.log(navigator.userAgent)
 		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 			if (cardMain.card) {
-				document.addEventListener('click', (e) => {
-					cardMain.card.forEach(card => {
-						if (e.target !== card) {
-							cardMain.card.forEach(card => card.classList.remove('is-active'))
+				cardMain.card.forEach(card => {
+					card.addEventListener('click', e => {
+						e.stopPropagation();
+						e.preventDefault();
+						if (card.classList.contains('is-active')) {
+							cardMain.card.forEach(card => { card.classList.remove('is-active') })
 						} else {
-							if (card.classList.contains('is-active')) {
-								card.classList.remove('is-active');
-							} else {
-								card.classList.add('is-active');
-							}
+							cardMain.card.forEach(card => { card.classList.remove('is-active') })
+							card.classList.add('is-active');
+						}
+
+					})
+
+					document.addEventListener('click', (e) => {
+						if (e.target !== card) {
+							cardMain.card.forEach(card => { card.classList.remove('is-active') })
 						}
 					})
+				})
 
 
-				})
-			}
-		} else {
-			if (cardMain.card) {
-				cardMain.card.forEach(card => {
-					card.addEventListener('mouseenter', () => {
-						card.classList.add('is-active');
-					})
-					card.addEventListener('mouseleave', () => {
-						card.classList.remove('is-active');
-					})
-				})
 			}
 		}
 	}
